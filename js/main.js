fefd2f75257b68a84867ffce38bd4f291c8f2860
Yuriy Zudikhin <uzudihin@gmail.com>
2020-01-13 17:53:15 +0300

    lol

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 "use strict";
-let headerBtn = document.querySelector('.header__btn');
-let headerMenu = document.querySelector('.header__menu');
-let headerIcons = document.querySelector('.header__icons');
-let header = document.querySelector('.header');
+let main = document.querySelector(".main");
+let headerBtn = document.querySelector(".header__btn");
+let headerMenu = document.querySelector(".header__menu");
+let headerIcons = document.querySelector(".header__icons");
+let headerContent = document.querySelector(".header__content");
 headerBtn.addEventListener("click", openBurger);
 
 function openBurger() {
-	if(this.classList.contains('active')) {
-		this.classList.remove('active');
-		headerMenu.classList.remove('active');
-		headerIcons.classList.remove('active');
-		header.classList.remove('active');
-	} else {
-		this.classList.add('active');
-		headerMenu.classList.add('active');
-		headerIcons.classList.add('active');
-		header.classList.add('active');
-	}
+  if (this.classList.contains("active")) {
+    this.classList.remove("active");
+    headerMenu.classList.remove("active");
+    headerIcons.classList.remove("active");
+    headerContent.classList.remove("active");
+    main.classList.remove("active");
+  } else {
+    this.classList.add("active");
+    headerMenu.classList.add("active");
+    headerIcons.classList.add("active");
+    headerContent.classList.add("active");
+    main.classList.add("active");
+  }
 }
