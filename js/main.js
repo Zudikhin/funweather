c0ff90f1b71712de701d64605bab83c1b49ef2f5
Yuriy Zudikhin <uzudihin@gmail.com>
2020-01-14 13:02:00 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let headerBtn = document.querySelector(".header__btn");
 let headerMenu = document.querySelector(".header__menu");
 let headerIcons = document.querySelector(".header__icons");
 let headerContent = document.querySelector(".header__content");
+let header = document.querySelector(".header");
 headerBtn.addEventListener("click", openBurger);
 
 function openBurger() {
@@ -13,11 +14,13 @@ function openBurger() {
     headerIcons.classList.remove("active");
     headerContent.classList.remove("active");
     main.classList.remove("active");
+    header.classList.remove("active");
   } else {
     this.classList.add("active");
     headerMenu.classList.add("active");
     headerIcons.classList.add("active");
     headerContent.classList.add("active");
     main.classList.add("active");
+    header.classList.add("active");
   }
 }
