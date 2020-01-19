fc2d8899c57a8af9a7d8862c8b5f785794a125b3
Yuriy Zudikhin <uzudihin@gmail.com>
2020-01-19 17:32:32 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -25,9 +25,70 @@ function openBurger() {
   }
 }
 
-let radius = document.querySelectorAll(".radius");
-for (let i = 0; i < radius.length; i++) {
-  let item = radius[i];
-  item.classList.add("")
-  console.log(item);
+let item = document.querySelectorAll('.item');
+let prev = document.querySelector('#prev');
+let next = document.querySelector('#next');
+
+next.addEventListener("click", () => {
+    let lastImgItem = item[item.length - 1].childNodes[0];
+    for (let i = 0; i < item.length; i++) { 
+        if (i < item.length - 1) {
+            let images = item[i].childNodes[0];
+            item[i].childNodes[0].remove();
+            item[i + 1].append(images);
+        } else if (i == item.length - 1) {
+            item[0].append(lastImgItem);
+        }
+    }
+});
+
+prev.addEventListener("click", () => {
+    let firstImgItem = item[0].childNodes[0];
+    for (let i = item.length - 1; i >= 0; i--) {
+        if (i > 0) {
+            let images = item[i].childNodes[0];
+            item[i].childNodes[0].remove();
+            item[i - 1].append(images);
+        } else if (i == 0) {
+            item[item.length - 1].append(firstImgItem);
+        }
+    }
+});
+
+for (let i = 0; i < item.length; i++) {
+    item[i].addEventListener("touchstart", startTouch);
+    item[i].addEventListener("touchmove", moveTouch);
 }
+
+let initialX = null;
+
+let currentSlide = 1;
+
+function startTouch(e) {
+    initialX = e.touches[0].clientX;
+}
+
+function moveTouch(e) {
+  if (initialX === null) {
+    return;
+  }
+  let currentX = e.touches[0].clientX;
+  let diffX = initialX - currentX;
+  if (diffX > 0) {
+    moveToSlide(currentSlide + 1);
+  } else {
+    moveToSlide(currentSlide - 1);
+  }
+  initialX = null;
+  e.preventDefault();
+};
+
+function moveToSlide(n) {
+    item[currentSlide].className = 'item';
+    currentSlide = (n + item.length) % item.length;
+    item[currentSlide].className = 'item active';
+}
+
+
+
+
